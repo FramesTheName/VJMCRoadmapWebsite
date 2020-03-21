ec65d9b4f4098a863a9b076cf77ce4ffd08948d3
FramesTheName <frame.mlf@gmail.com>
2020-03-21 10:52:42 -0600

    Made Certifications

--- RoadMapWebsite/src/app/app.module.ts
+++ RoadMapWebsite/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from './material/material.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,7 +29,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- RoadMapWebsite/src/app/career-match-page/career-match-page.component.ts
+++ RoadMapWebsite/src/app/career-match-page/career-match-page.component.ts
@@ -1,9 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
 import { Location } from '@angular/common';
+import { MatChipInputEvent } from '@angular/material/chips';
+import {MatAutocompleteSelectedEvent, MatAutocomplete} from '@angular/material/autocomplete';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 
 import { Skill } from '../core/models/skill';
 import { SkillService } from '../core/model-services/skill.service';
+import { Certification } from '../core/models/certification';
+import { CertificationService } from '../core/model-services/certification.service'
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+
+
+export interface certification {
+  name: string;
+}
 
 @Component({
   selector: 'app-career-match-page',
@@ -12,26 +24,87 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 })
 export class CareerMatchPageComponent implements OnInit {
   skills: Skill[];
+  certifications: Certification[];
   userForm = this.fb.group({
     MOSNumber: [''],
     firstSkill: ['', Validators.required],
     secondSkill: ['', Validators.required],
     thirdSkill: ['', Validators.required],
   });
+  visible = true;
+  selectable = true;
+  removable = true;
+  separatorKeysCodes: number[] = [ENTER, COMMA];
+  certCtrl = new FormControl();
+  filteredCertifications: Observable<string[]>;
+  myCertifications: string[] = [];
+  allCertifications: string[] = [];
+
+  @ViewChild('certificationInput') certificationInput: ElementRef<HTMLInputElement>;
+  @ViewChild('auto') matAutocomplete: MatAutocomplete;
+
+  add(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    // Add our certification
+    if ((value || '').trim()) {
+      this.myCertifications.push(value.trim());
+    }
+
+    // Reset the input value
+    if (input) {
+      input.value = '';
+    }
+
+    this.certCtrl.setValue(null);
+  }
+
+  remove(certification: string): void {
+    const index = this.myCertifications.indexOf(certification);
+
+    if (index >= 0) {
+      this.myCertifications.splice(index, 1);
+    }
+  }
+
+  selected(event: MatAutocompleteSelectedEvent): void {
+    this.myCertifications.push(event.option.viewValue);
+    this.certificationInput.nativeElement.value = '';
+    this.certCtrl.setValue(null);
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.allCertifications.filter(certification => certification.toLowerCase().indexOf(filterValue) === 0);
+  }
+
 
   constructor(
     private location: Location,
     private skillService: SkillService,
-    private fb: FormBuilder
-        ) { }
+    private fb: FormBuilder,
+    private certificationService: CertificationService,
+        ) { 
+          this.filteredCertifications = this.certCtrl.valueChanges.pipe(
+            startWith(null),
+            map((certification: string | null) => certification ? this._filter(certification) : this.allCertifications.slice()));
+        }
 
   ngOnInit() {
     this.getSkills();
-    this.buildForms
+    this.getCerts();
   }
 
-  buildForms(){
+  getmyCertifications(){
+    
+  }
 
+  getCerts() {
+    this.certificationService.getCertifications()
+      .subscribe(certifications => this.certifications = certifications);
+      this.allCertifications = this.certifications.map((a => a.name));
   }
 
   getSkills() {
