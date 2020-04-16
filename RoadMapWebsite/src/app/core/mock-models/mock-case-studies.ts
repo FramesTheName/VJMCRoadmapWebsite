a04178e50d4e1f0c21d9f424f5dc9e6ff52cba89
FramesTheName <frame.mlf@gmail.com>
2020-04-16 08:57:28 -0600

    Added Ron case study complete

--- RoadMapWebsite/src/app/core/mock-models/mock-case-studies.ts
+++ RoadMapWebsite/src/app/core/mock-models/mock-case-studies.ts
@@ -24,8 +24,8 @@ export const CASESTUDIES: CaseStudy[] = [
       1994,
       2005
     ],
-    "story": "Coming Soon. . .",
-    "video": "https://www.youtube.com/embed/j9WBdThrawA"
+    "story": "Ron served in the Navy as an Electronics Technician from 1994-2005. Now he works for Cisco as Solutions Architect. Whilie serving in the military Ron learned how to work as a team and rapidly adapt to change. His military experience helps him suceed as a Solutions Architect, where he helps people who are directly impacted by natural disasters. Ron's advice to veterans looking to get into tech is to study, go to school, and start building a professional network by attending job fairs, career days, and expos.",
+    "video": "https://www.youtube.com/embed/3WoLeQcGVvQ"
   },
   {
     "id": 3,
